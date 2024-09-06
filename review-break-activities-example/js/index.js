705be04aac774a9373cbc9a32e68507a6c67ac2f
Tom Maurer <tmaurer@nait.ca>
2024-09-06 09:45:05 -0600

    Class 2 example

--- review-break-activities-example/js/index.js
+++ review-break-activities-example/js/index.js
@@ -6,3 +6,26 @@ HTML for list topic list item
     NEW TOPIC HERE
 </li>
 */
+
+let topicList = document.querySelector(".topics-list");
+let newTopicForm = document.querySelector(".new-topic-form");
+
+const addTopicToPage = (topicName, topicListElement) => {
+  let newTopicElement = `<li class="list-group-item">${topicName}</li>`;
+
+  topicListElement.innerHTML += newTopicElement;
+  console.log(topicListElement.innerHTML);
+};
+
+newTopicForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let topicInput = event.target.elements["new-topic"];
+  let newTopic = topicInput.value;
+  if (newTopic === "") {
+    topicInput.classList.add("is-invalid");
+  } else {
+    topicInput.classList.remove("is-invalid");
+    addTopicToPage(newTopic, topicList);
+  }
+});
